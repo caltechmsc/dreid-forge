69397af531c3fda9f606385ddc0a897b19231371
Tony Kan <tonykan@caltech.edu>
2025-12-05 23:44:55 -0800

    test(model): Add unit tests for Atom struct methods and properties

--- src/model/atom.rs
+++ src/model/atom.rs
@@ -11,3 +11,45 @@ impl Atom {
         Self { element, position }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::model::types::Element;
+
+    #[test]
+    fn atom_new_and_fields() {
+        let pos = [0.0_f64, 1.5, -2.25];
+        let a = Atom::new(Element::C, pos);
+        assert_eq!(a.element, Element::C);
+        assert_eq!(a.position, pos);
+    }
+
+    #[test]
+    fn atom_clone_and_eq() {
+        let a = Atom::new(Element::O, [1.0, 2.0, 3.0]);
+        let b = a.clone();
+        assert_eq!(a, b);
+        let mut c = b.clone();
+        c.position[0] = 9.0;
+        assert_ne!(a, c);
+    }
+
+    #[test]
+    fn atom_debug_contains_fields() {
+        let a = Atom::new(Element::Na, [0.0, 0.0, 0.0]);
+        let dbg = format!("{:?}", a);
+        assert!(dbg.contains("Atom"));
+        assert!(dbg.contains("Na"));
+        assert!(dbg.contains("position"));
+    }
+
+    #[test]
+    fn atom_position_precision_preserved() {
+        let pos = [1e-12_f64, -1e6, 3.141592653589793];
+        let a = Atom::new(Element::H, pos);
+        assert_eq!(a.position[0], pos[0]);
+        assert_eq!(a.position[1], pos[1]);
+        assert_eq!(a.position[2], pos[2]);
+    }
+}
