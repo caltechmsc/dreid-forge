fb47b1e7365aa5eddfc9bafef6e4d528305a7903
Tony Kan <tonykan@caltech.edu>
2025-12-07 18:54:12 -0800

    feat(io): Implement PDB writer functionality for modular IO subsystem

---
+++ src/io/pdb/writer.rs
@@ -0,0 +1,9 @@
+use crate::io::{error::Error, util};
+use crate::model::system::System;
+use bio_forge as bf;
+use std::io::Write;
+
+pub fn write<W: Write>(writer: W, system: &System) -> Result<(), Error> {
+    let topo = util::to_bio_topology(system)?;
+    bf::io::write_pdb_topology(writer, &topo).map_err(Error::from)
+}
