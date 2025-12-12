e45a6765e453fa3741655295baafdfc99ef8147f
Tony Kan <tonykan@caltech.edu>
2025-12-11 20:34:09 -0800

    refactor(lib): Organize module imports and public exports for clarity and structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
-pub mod forge;
+mod forge;
+mod model;
+
 pub mod io;
-pub mod model;
+
+pub use model::atom::Atom;
+pub use model::system::{Bond, System};
+pub use model::types::{BondOrder, Element, ParseBondOrderError, ParseElementError};
+
+pub use model::topology::{
+    AnglePotential, AtomParam, BondPotential, DihedralPotential, ForgedSystem, HBondPotential,
+    ImproperPotential, Potentials, VdwPairPotential,
+};
+
+pub use model::metadata::{
+    AtomResidueBuilder, AtomResidueInfo, BioMetadata, ResidueCategory, ResiduePosition,
+    StandardResidue,
+};
+
+pub use forge::{
+    AnglePotentialType, BondPotentialType, ChargeMethod, ForgeConfig, QeqConfig, SolverOptions,
+    VdwPotentialType, forge,
+};
+
+pub use forge::Error as ForgeError;
