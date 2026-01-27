bf07b489c1f5bf142d4cc7cd17eb442aa51d7781
Tony Kan <tonykan@caltech.edu>
2026-01-27 03:12:29 -0800

    refactor(cli): Remove LAMMPS configuration support

--- src/bin/dforge/config/mod.rs
+++ src/bin/dforge/config/mod.rs
@@ -1,11 +1,9 @@
 mod bio;
 mod forge;
-mod lammps;
 
 pub use bio::{
     build_clean_config, build_protonate_config, build_solvate_config, build_topology_config,
 };
 pub use forge::{build_bio_forge_config, build_chem_forge_config};
-pub use lammps::build_lammps_config;
 
 pub use crate::util::convert::potential_display_names as potential_names;
